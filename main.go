27b7450562790f84cfae11bf982c9f7870788f84
Emanuele <107957269+emanuelefaja@users.noreply.github.com>
2024-12-04 15:57:37 +0100

    Support header component in main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
@@ -40,7 +41,7 @@ func main() {
 	}
 
 	// Create templates with custom functions
-	templates := template.Must(template.New("").Funcs(template.FuncMap{
+	funcMap := template.FuncMap{
 		"times": func(n int) []int {
 			var result []int
 			for i := 0; i < n; i++ {
@@ -51,7 +52,16 @@ func main() {
 		"add": func(a, b int) int {
 			return a + b
 		},
-	}).ParseGlob("ui/*.html"))
+		"dict": dict,
+	}
+
+	templates := template.Must(template.New("").Funcs(funcMap).ParseFiles(
+		"ui/components/header.html",
+		"ui/home.html",
+		"ui/settings.html",
+		"ui/login.html",
+		"ui/register.html",
+	))
 
 	// Handle static files
 	fs := http.FileServer(http.Dir("static"))
@@ -196,4 +206,19 @@ func main() {
 	// Start server
 	log.Println("Server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func dict(values ...interface{}) (map[string]interface{}, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call")
+	}
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
 }
