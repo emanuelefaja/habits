ce634dd3174cc3f9e372fde5e3f3387a8ab3a98f
Emanuele <107957269+emanuelefaja@users.noreply.github.com>
2024-12-07 15:47:10 +0100

    Added first name and last name to db init

--- models/db.go
+++ models/db.go
@@ -10,6 +10,8 @@ func InitDB(db *sql.DB) error {
 	_, err := db.Exec(`
 		CREATE TABLE IF NOT EXISTS users (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			first_name TEXT NOT NULL,
+			last_name TEXT NOT NULL,
 			email TEXT UNIQUE NOT NULL,
 			password_hash TEXT NOT NULL,
 			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
